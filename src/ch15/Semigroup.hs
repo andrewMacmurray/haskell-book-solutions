e4e21eafc31b1d93b11093713d72e48d4f18f9d9
Andrew MacMurray <a.macmurray@icloud.com>
2017-03-16 22:13:16 +0000

    complete semigroup exercises

--- src/ch15/Semigroup.hs
+++ src/ch15/Semigroup.hs
@@ -176,6 +176,149 @@ type BoolDisjAssoc =
   Bool
 
 
+
+data Or a b =
+    Fst a
+  | Snd b
+  deriving (Eq, Show)
+
+instance Semigroup (Or a b) where
+  Snd a <> _     = Snd a
+  Fst _ <> Snd a = Snd a
+  Fst a <> _     = Fst a
+
+orGen :: (Arbitrary a, Arbitrary b) => Gen (Or a b)
+orGen = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [ Fst a, Snd b ]
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
+  arbitrary = orGen
+
+type OrAssoc =
+  Or String String ->
+  Or String String ->
+  Or String String ->
+  Bool
+
+
+
+newtype Combine a b =
+  Combine { unCombine :: (a -> b) }
+
+instance Semigroup b => Semigroup (Combine a b) where
+  (Combine f) <> (Combine g) = Combine (f <> g)
+
+
+
+newtype Comp a =
+  Comp { unComp :: (a -> a) }
+
+instance Semigroup (Comp a) where
+  Comp f <> Comp g = Comp (g . f)
+
+
+genFunc :: (CoArbitrary a, Arbitrary a) => Gen (a -> a)
+genFunc = arbitrary
+
+genComp :: (CoArbitrary a, Arbitrary a) => Gen (Comp a)
+genComp = do
+  f <- genFunc
+  return (Comp { unComp = f })
+
+
+
+
+data Validation a b =
+    Failure' a
+  | Success' b
+  deriving (Eq, Show)
+
+instance Semigroup (Validation a b) where
+  (Success' a) <> (Success' _) = Success' a
+  (Success' _) <> (Failure' a) = Failure' a
+  (Failure' a) <> _            = Failure' a
+
+validationGen :: (Arbitrary a, Arbitrary b) => Gen (Validation a b)
+validationGen = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [ Failure' a, Success' b ]
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Validation a b) where
+  arbitrary = validationGen
+
+type ValidationAssoc =
+  Validation String Int ->
+  Validation String Int ->
+  Validation String Int ->
+  Bool
+
+
+
+newtype AccumulateRight a b =
+  AccumulateRight (Validation a b)
+  deriving (Eq, Show)
+
+
+instance Semigroup b => Semigroup (AccumulateRight a b) where
+  (<>) (AccumulateRight (Success' b))
+       (AccumulateRight (Success' b'))  = AccumulateRight $ Success' (b <> b')
+  (<>) (AccumulateRight (Success' _))
+       (AccumulateRight (Failure' a))   = AccumulateRight $ Failure' a
+  (<>) (AccumulateRight (Failure' a)) _ = AccumulateRight $ Failure' a
+
+accumRightGen :: (Arbitrary a, Arbitrary b) => Gen (AccumulateRight a b)
+accumRightGen = do
+  a <- validationGen
+  b <- validationGen
+  frequency [ (10, return $ AccumulateRight a )
+            , (1,  return $ AccumulateRight b)
+            ]
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (AccumulateRight a b) where
+  arbitrary = accumRightGen
+
+type AccumRightAssoc =
+  AccumulateRight String (Sum Int) ->
+  AccumulateRight String (Sum Int) ->
+  AccumulateRight String (Sum Int) ->
+  Bool
+
+
+newtype AccumulateBoth a b =
+  AccumulateBoth (Validation a b)
+  deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (AccumulateBoth a b) where
+  (<>) (AccumulateBoth (Success' a))
+       (AccumulateBoth (Success' b)) = AccumulateBoth $ Success' (a <> b)
+  (<>) (AccumulateBoth (Success' _))
+       (AccumulateBoth (Failure' a)) = AccumulateBoth $ Failure' a
+  (<>) (AccumulateBoth (Failure' a))
+       (AccumulateBoth (Success' _)) = AccumulateBoth $ Failure' a
+  (<>) (AccumulateBoth (Failure' a))
+       (AccumulateBoth (Failure' b)) = AccumulateBoth $ Failure' (a <> b)
+
+
+accumBothGen :: (Arbitrary a, Arbitrary b) => Gen (AccumulateBoth a b)
+accumBothGen = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [ AccumulateBoth a, AccumulateBoth b ]
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (AccumulateBoth a b) where
+  arbitrary = accumBothGen
+
+
+type AccumBothAssoc =
+  AccumulateBoth String (Product Int) ->
+  AccumulateBoth String (Product Int) ->
+  AccumulateBoth String (Product Int) ->
+  Bool
+
+
 semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
 semigroupAssoc a c b = (a <> (b <> c)) == ((a <> b) <> c)
 
@@ -188,3 +331,7 @@ main = do
   quickCheck (semigroupAssoc :: FourAssoc)
   quickCheck (semigroupAssoc :: BoolConjAssoc)
   quickCheck (semigroupAssoc :: BoolDisjAssoc)
+  quickCheck (semigroupAssoc :: OrAssoc)
+  quickCheck (semigroupAssoc :: ValidationAssoc)
+  quickCheck (semigroupAssoc :: AccumRightAssoc)
+  quickCheck (semigroupAssoc :: AccumBothAssoc)
