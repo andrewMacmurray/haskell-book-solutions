c3a3eeb6ac4c04be306f0d96b66f88e54e9b3fdd
Andrew MacMurray <a.macmurray@icloud.com>
2017-04-16 10:57:40 +0100

    implement zipList applicative

--- src/ch17/List.hs
+++ src/ch17/List.hs
@@ -29,26 +29,52 @@ genList = do
   a <- arbitrary
   l <- genList
   frequency [ (1, return Nil)
-            , (5, return (Cons a l)) ]
+            , (2, return (Cons a l)) ]
 
 instance Arbitrary a => Arbitrary (List a) where
   arbitrary = genList
 
+newtype ZipList' a =
+  ZipList' (List a)
+  deriving (Eq, Show)
+
+instance Functor ZipList' where
+  fmap f (ZipList' xs) = ZipList' (fmap f xs)
+
+instance Applicative ZipList' where
+  pure x = ZipList' (Cons x Nil)
+  (<*>) (ZipList' fs) (ZipList' xs) = ZipList' $ (zipListWith fs xs)
+
+zipListWith :: List (a -> b) -> List a -> List b
+zipListWith _ Nil = Nil
+zipListWith Nil _ = Nil
+zipListWith (Cons f Nil) (Cons x xs) = Cons (f x) (pure f <*> xs)
+zipListWith (Cons f fs) (Cons x Nil) = Cons (f x) (fs <*> pure x)
+zipListWith (Cons f fs) (Cons x xs)  = Cons (f x) (zipListWith fs xs)
+
+instance Arbitrary a => Arbitrary (ZipList' a) where
+  arbitrary = ZipList' <$> arbitrary
+
+instance Eq a => EqProp (ZipList' a) where
+  xs =-= ys = xs'`eq` ys'
+    where xs' = let (ZipList' l) = xs
+                in take' 1000 l
+          ys' = let (ZipList' l) = ys
+                in take' 1000 l
+
 instance Eq a => EqProp (List a) where
     xs =-= ys = xs' `eq` ys'
         where xs' = let l = xs
-                    in take' 3000 l
+                    in take' 1000 l
               ys' = let l = ys
-                    in take' 3000 l
+                    in take' 1000 l
 
-take' :: Integer -> List a -> List a
-take' n xs = go n xs Nil
-  where
-    go _ Nil acc         = acc
-    go n' (Cons h t) acc =
-      if n' == 0
-      then acc
-      else go (n' - 1) t (Cons h acc)
+take' :: Int -> List a -> List a
+take' 0 _ = Nil
+take' _ Nil = Nil
+take' n (Cons x xs) = Cons x (take' (n - 1) xs)
 
-main :: IO ()
-main = quickBatch $ applicative (Cons ("a", "b", "c") Nil)
+listSpec :: IO ()
+listSpec = do
+  quickBatch $ applicative (Cons ("a", "b", "c") Nil)
+  quickBatch $ applicative (ZipList' (Cons ("a", "b", "b") Nil))
