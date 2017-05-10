8c90011345fe17127bf1a6c2c9e9eb4c5cf61695
Andrew MacMurray <a.macmurray@icloud.com>
2017-05-10 16:26:53 +0100

    library functions for foldable

--- src/ch20/Library.hs
+++ src/ch20/Library.hs
@@ -11,31 +11,32 @@ product' :: (Foldable t, Num a) => t a -> a
 product' = getProduct . foldMap Product
 
 elem' :: (Foldable t, Eq a) => a -> t a -> Bool
-elem' x xs =
-  xs
-    |> toList
-    |> map ((==) x)
-    |> foldMap Any
-    |> getAny
-
-
-newtype Min a = Min { getMin :: Maybe a } deriving (Eq, Ord, Show)
-
-instance Ord a => Monoid (Min a) where
-  mempty = Min Nothing
-  mappend a (Min Nothing) = a
-  mappend (Min Nothing) a = a
-  mappend (Min (Just x)) (Min (Just y))
-    | x <= y = Min $ Just x
-    | otherwise = Min $ Just y
+elem' x = getAny . foldMap (\y -> Any (y == x))
 
 minimum' :: (Foldable t, Ord a) => t a -> Maybe a
-minimum' xs
+minimum' = compare'' min
+
+maximum' :: (Foldable t, Ord a) => t a -> Maybe a
+maximum' = compare'' max
+
+compare'' :: (Foldable t, Ord a) => (a -> a -> a) -> t a -> Maybe a
+compare'' compare' xs
   | null xs   = Nothing
-  | otherwise = x
-    where
-      x = xs
-        |> toList
-        |> map Just
-        |> foldMap Min
-        |> getMin
+  | otherwise = xs' |> foldr compare' (head xs') |> Just
+  where
+    xs' = toList xs
+
+null' :: (Foldable t) => t a -> Bool
+null' x = length x == 0
+
+length' :: (Foldable t) => t a -> Int
+length' = foldr (\_ a -> a + 1) 0
+
+toList' :: (Foldable t) => t a -> [a]
+toList' = foldr (:) []
+
+fold' :: (Foldable t, Monoid m) => t m -> m
+fold' = foldMap id
+
+foldMap' :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
+foldMap' f = foldr (\b a -> (f b) <> a) mempty
