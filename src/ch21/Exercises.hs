39b17cc63cb87cee8e2772f084a819909e3b8c1c
Andrew MacMurray <a.macmurray@icloud.com>
2017-05-12 14:09:56 +0100

    complete exercises for traversable

--- src/ch21/Exercises.hs
+++ src/ch21/Exercises.hs
@@ -4,6 +4,7 @@ import Test.QuickCheck
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
 import Data.Monoid
+import Control.Applicative
 
 newtype Identity a =
   Identity a
@@ -92,12 +93,120 @@ instance Traversable List where
   traverse _ Nil = pure Nil
   traverse f (Cons x xs) = Cons <$> f x <*> traverse f xs
 
+genList :: Arbitrary a => Gen (List a)
+genList = do
+  a <- arbitrary
+  b <- genList
+  frequency [ (3, return $ Cons a b)
+            , (1, return Nil)
+            ]
+
+instance Arbitrary a => Arbitrary (List a) where
+  arbitrary = genList
+
+instance Eq a => EqProp (List a) where
+  (=-=) = eq
+
+
+data Three a b c =
+  Three a b c
+  deriving (Eq, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three a b c) = Three a b (f c)
+
+instance Foldable (Three a b) where
+  foldMap f (Three a b c) = f c
+
+instance Traversable (Three a b) where
+  traverse f (Three a b c) = (Three a b) <$> f c
+
+instance (Arbitrary a
+        , Arbitrary b
+        , Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return $ Three a b c
+
+instance (Eq a, Eq b, Eq c) => EqProp (Three a b c) where
+  (=-=) = eq
+
+
+data Three' a b =
+  Three' a b b
+  deriving (Eq, Show)
+
+instance Functor (Three' a) where
+  fmap f (Three' a b c) = Three' a (f b) (f c)
+
+instance Foldable (Three' a) where
+  foldMap f (Three' a b c) = (f b) <> (f c)
+
+instance Traversable (Three' a) where
+  traverse f (Three' a b c) = (Three' a) <$> (f b) <*> (f c)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Three' a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return $ Three' a b c
+
+instance (Eq a, Eq b) => EqProp (Three' a b) where
+  (=-=) = eq
+
+
+
+data Tree a =
+    Empty
+  | Leaf a
+  | Node (Tree a) a (Tree a)
+  deriving (Eq, Show)
+
+instance Functor Tree where
+  fmap _ Empty    = Empty
+  fmap f (Leaf a) = Leaf (f a)
+  fmap f (Node xs x ys) = Node (fmap f xs) (f x) (fmap f ys)
+
+instance Foldable Tree where
+  foldMap _ Empty = mempty
+  foldMap f (Leaf a) = f a
+  foldMap f (Node xs y ys) = (foldMap f xs) <> (f y) <> (foldMap f ys)
+
+instance Traversable Tree where
+  traverse _ Empty = pure Empty
+  traverse f (Leaf a) = Leaf <$> f a
+  traverse f (Node xs y ys) = liftA3 Node (traverse f xs) (f y) (traverse f ys)
+
+
+treeGen :: Arbitrary a => Gen (Tree a)
+treeGen = do
+  xs <- treeGen
+  ys <- treeGen
+  a <- arbitrary
+  elements [ Leaf a
+           , Empty
+           , Node xs a ys
+           ]
+
+instance Arbitrary a => Arbitrary (Tree a) where
+  arbitrary = treeGen
+
+instance Eq a => EqProp (Tree a) where
+  (=-=) = eq
+
 
 type Trigger = (Int, Int, [Int])
 
-traversableSpec :: IO ()
-traversableSpec = do
+main :: IO ()
+main = do
   let trigger = undefined
   quickBatch $ traversable (trigger :: Identity Trigger)
   quickBatch $ traversable (trigger :: Constant Int Trigger)
   quickBatch $ traversable (trigger :: Optional Trigger)
+  quickBatch $ traversable (trigger :: List Trigger)
+  quickBatch $ traversable (trigger :: Three Trigger Trigger Trigger)
+  quickBatch $ traversable (trigger :: Three' Trigger Trigger)
+  quickBatch $ traversable (trigger :: Tree Trigger)
